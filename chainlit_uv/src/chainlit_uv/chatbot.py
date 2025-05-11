ef14e957fb3336b0e71b774fa2ffe67e14febf70
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-05-11 19:10:27 +0500

    Integrted the gemini gpt

--- chainlit_uv/src/chainlit_uv/chatbot.py
+++ chainlit_uv/src/chainlit_uv/chatbot.py
@@ -1,9 +1,51 @@
+import os
 import chainlit as cl
 
-@cl.on_message
-async def main(message: cl.Message):
-    # Our custom logic goes here...
-    # Send a fake response back to the user
-    await cl.Message(
-        content=f"Received: {message.content}",
-    ).send()
+from dotenv import load_dotenv, find_dotenv
+from agents import Agent, RunConfig, AsyncOpenAI, OpenAIChatCompletionsModel, Runner
+
+load_dotenv(find_dotenv())
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# Setup 1: Provider
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.google.com/v1beta/openai/",
+)
+
+# Setup 2: Model
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    provider=provider
+)
+
+# Config: Defined at Run Level
+run_config = RunConfig(
+    model=model,
+    model_provider=provider,
+    tracing_disabled=True
+)
+
+# Setup 3: Agent
+agent = Agent(
+    instructions="You are a helpful assistant that can answer questions and",
+    name="Panaversity Support Agent"
+)
+
+# Setup 4: Run
+result = Runner.run_sync(
+    input="What is the capital of Turkey?",
+    run_config=run_config   
+)
+
+print(result.final_output)
+
+
+# @cl.on_message
+# async def main(message: cl.Message):
+#     # Our custom logic goes here...
+#     # Send a fake response back to the user
+#     await cl.Message(
+#         content=f"Received: {message.content}",
+#     ).send()
